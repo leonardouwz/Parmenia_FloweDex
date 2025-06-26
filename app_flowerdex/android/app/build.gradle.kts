9bc263383ccb92a5dbfc627c58a9549150531810
leonardouwz <lpacharig@ulasalle.edu.pe>
2025-06-26 02:35:47 -0500

    Actualizando dependencias

--- app_flowerdex/android/app/build.gradle.kts
+++ app_flowerdex/android/app/build.gradle.kts
@@ -6,6 +6,10 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+
+    id("com.android.application")
+    // Add the Google services Gradle plugin
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,4 +53,6 @@ flutter {
 
 dependencies {
     implementation("androidx.multidex:multidex:2.0.1")
+    // Import the Firebase BoM
+    implementation(platform("com.google.firebase:firebase-bom:33.15.0"))
 }
